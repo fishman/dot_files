2c696e48742bdfeb4f3dae9dbba99a729a5ab801
Reza Jelveh <toyb0mb@gmx.net>
2017-03-12 23:01:43 +0100

    various update

--- zsh/.zsh.d/zaw/sources/history.zsh
+++ zsh/.zsh.d/zaw/sources/history.zsh
@@ -1,10 +1,17 @@
 zmodload zsh/parameter
 
 function zaw-src-history() {
-    cands_assoc=("${(@kv)history}")
+    if zstyle -t ':filter-select' hist-find-no-dups ; then
+        candidates=(${(@vu)history})
+        options=("-m" "-s" "${BUFFER}")
+    else
+        cands_assoc=("${(@kv)history}")
+        # have filter-select reverse the order (back to latest command first).
+        # somehow, `cands_assoc` gets reversed while `candidates` doesn't. 
+        options=("-r" "-m" "-s" "${BUFFER}")
+    fi
     actions=("zaw-callback-execute" "zaw-callback-replace-buffer" "zaw-callback-append-to-buffer")
     act_descriptions=("execute" "replace edit buffer" "append to edit buffer")
-    options=("-r" "-m" "-s" "${BUFFER}")
 
     if (( $+functions[zaw-bookmark-add] )); then
         # zaw-src-bookmark is available
