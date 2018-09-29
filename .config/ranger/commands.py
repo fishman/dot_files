ab3f4ed76f36434c89ed963a42fd10fcc03f162d
Reza Jelveh <toyb0mb@gmx.net>
2018-09-29 22:30:51 +0200

    ranger

---
+++ .config/ranger/commands.py
@@ -0,0 +1,44 @@
+import os
+from ranger.api.commands import *
+from ranger.core.loader import CommandLoader
+
+class extracthere(Command):
+    def execute(self):
+        """ Extract copied files to current directory """
+        copied_files = tuple(self.fm.env.copy)
+
+        if not copied_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.env.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = copied_files[0]
+        cwd = self.fm.env.cwd
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.env.copy.clear()
+        self.fm.env.cut = False
+        if len(copied_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in copied_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+class empty(Command):
+    """:empty
+
+    Empties the trash directory ~/.Trash
+    """
+
+    def execute(self):
+        self.fm.run("rm -rf /home/myname/.Trash/{*,.[^.]*}")
+
