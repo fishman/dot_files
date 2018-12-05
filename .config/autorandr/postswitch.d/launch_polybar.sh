b5a8312cddc6a90b5f2e56c918e0fdc5a01ecf97
Reza Jelveh <toyb0mb@gmx.net>
2018-12-05 22:44:36 +0100

    fix polybar automatically when switch screen layout

---
+++ .config/autorandr/postswitch.d/launch_polybar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload example &
+    done
+else
+    polybar --reload example &
+fi
